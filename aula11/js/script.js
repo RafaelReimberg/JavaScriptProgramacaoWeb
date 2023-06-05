73812817ae312421701d0775f1b14024b61e017b
RafaelReimberg <50966566+RafaelReimberg@users.noreply.github.com>
2023-06-04 21:26:19 -0300

    Update script.js

---
+++ aula11/js/script.js
@@ -0,0 +1,14 @@
+/* ARRAY
+
+Os arrays JavaScript são usados para armazenar vário valores
+em um única variavél. Diferente dos objetos, que funcionam com propriedades ou "nomes" que você dá paras os itens dentro dele, os arrays não possuem propriedades. O item dentro dele é encontrado pela posição.
+
+Imagine um array como uma lista de itens, com controles, por ordem de posição dentro dele.
+
+Ex: const lista = ["arroz", "feijão", "macarrão", "leite"];
+
+A lista = [0] (lista na posição 0) vai conter o valor "arroz".
+A lista = [1] (lista na posição 1) vai conter o valor "feijão".
+
+E assim por diante.
+*/
