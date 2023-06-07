604be63bbe04565346a576f471d011c0a68eba24
RafaelReimberg <50966566+RafaelReimberg@users.noreply.github.com>
2023-06-06 22:42:15 -0300

    Criado o exercício 15

--- aula15/js/script.js
+++ aula15/js/script.js
@@ -3,6 +3,19 @@
 Laços oferecem um jeito fácil e rápido de executar uma ação repetidas vezes.
 */
 
-for (let r = 0; r < 1001; r++){
+/*for (let r = 0; r < 1001; r++){
     document.getElementById("texto").innerHTML += r + ", ";
+}*/
+
+var year = new Date().getFullYear();
+
+for (let ano = year; ano >= 1900; ano--){
+    document.getElementById("ano").innerHTML += "<option value='" + ano + "'>" + ano + "</option>";
+}
+
+const carros = ["Gol", "Fusca", "Brasília", "Del Rey", "Chevette"];
+var tamanho = carros.length;
+
+for(let car = 0; car < tamanho; car++){
+    document.getElementById("carros").innerHTML += carros[car] + " - ";
 }
