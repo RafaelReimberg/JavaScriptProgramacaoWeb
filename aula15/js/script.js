29dcac2de3f39a6983810fe92455534a532ced1c
RafaelReimberg <50966566+RafaelReimberg@users.noreply.github.com>
2023-06-06 21:41:26 -0300

    atualizando exercicio 15

---
+++ aula15/js/script.js
@@ -0,0 +1,8 @@
+/* Laço de Repetição For
+
+Laços oferecem um jeito fácil e rápido de executar uma ação repetidas vezes.
+*/
+
+for (let r = 0; r < 1001; r++){
+    document.getElementById("texto").innerHTML += r + ", ";
+}
