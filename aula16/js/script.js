feccc1069f0b95f24eb9ed7ae7ac4bd7185cf257
Rafael Marcelo Reimberg <50966566+RafaelReimberg@users.noreply.github.com>
2025-09-17 17:42:19 -0300

    update

--- aula16/js/script.js
+++ aula16/js/script.js
@@ -14,20 +14,27 @@ setInterval(function, miliseconds)
 -> É o mesmo que serTimeout(), mas repete a execução da função continuamente.
 
 */
+let tempo;
 
 function ativarContagem() {
+    const tempoEl = document.getElementById('tempo');
+    tempoEl.style.color = "rgb(48, 74, 161)";
     //document.getElementById('tempo').innerHTML = "Começou a contar!";
     // Ativa a função apenas 1 vez quando der o tempo determinado
     //tempo = setTimeout(function () { document.getElementById('tempo').innerHTML = "Executou o setTimeout";}, 5000);
     tempo = setInterval(function () {
-        let cronometro = document.getElementById('tempo').innerHTML;
-        let soma = parseInt(cronometro) + 1;
+        // let cronometro = document.getElementById('tempo').innerHTML;
+        let cronometro = parseInt(tempoEl.innerHTML) || 0; 
+        // let soma = parseInt(cronometro) + 1;
+        let soma = cronometro + 1;
 
         if(soma === 60){
             document.getElementById('tempo').innerHTML = "Tempo esgotado!!!";
+            tempoEl.style.color = "red";
             pararContagem();
         }else{
-            document.getElementById('tempo').innerHTML = soma;
+            // document.getElementById('tempo').innerHTML = soma;
+            tempoEl.innerHTML = soma;
         }
         
     }, 1000);
@@ -37,5 +44,6 @@ function pararContagem() {
     //clearTimeout(tempo);
     //document.getElementById('tempo').innerHTML = "Parou a Contagem!";
     clearInterval(tempo);
+    document.getElementById('tempo').style.color = "rgb(1, 243, 252)";
     //document.getElementById('tempo').innerHTML = "Parou a Contagem!";
 }
